05d836772427565195b026a16e8b9f4b40efae25
parvathig2 <parvathig2811@gmail.com>
2022-01-16 13:18:31 +0530

    firstcommit

---
+++ src/App.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { cleanup, render, screen } from "@testing-library/react";
+import RawJson from "./Projects/FinalApp/components/header";
+
+
+
+
+
+
+
+cleanup()
+test('test to render the rawJson',()=>{
+    render(<RawJson></RawJson>);
+
+   screen.debug()
+})
---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+import ReactDOM from 'react-dom';
+import App from './Projects/FinalApp/App';
+import './index.css'
+ReactDOM.render(
+        <App/>
+,document.getElementById('root')
+);
+
+
